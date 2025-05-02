c6f614c726a094da3514ee3d24fb7c45b53a7712
vivan-sinha <vivan.sinha.003@gmail.com>
2025-05-02 14:02:28 -0700

    use hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { HashRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './Home'
 import ColoringBook from './coloring_book/ColoringBook'
 import ContactPage from './ContactPage'
-// import blackLogo from './assets/black_background.svg'
 import './App.css'
-import React from 'react'
-import { useEffect } from 'react'
+import React, { useEffect } from 'react'
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
-  const { pathname } = useLocation()
-  useEffect(() => {
-    window.scrollTo(0,0)
-  }, [pathname])
-  console.log('updated')
-
   return (
-    <Routes>
-      <Route path='*' element={<Home/>}/>
-      {/* <Route path='/contact' element={<ContactPage/>}/> */}
-      <Route path='/coloring-book' element={<ColoringBook></ColoringBook>}></Route>
-    </Routes>
-    
+    <HashRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path='*' element={<Home />} />
+        <Route path='/coloring-book' element={<ColoringBook />} />
+        {/* <Route path='/contact' element={<ContactPage />} /> */}
+      </Routes>
+    </HashRouter>
   )
 }
 
