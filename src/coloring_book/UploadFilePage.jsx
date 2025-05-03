80e6e201a178de41f66b8d13161daa342ec7a19f
vivan-sinha <vivan.sinha.003@gmail.com>
2025-05-02 17:25:10 -0700

    coloring-book ui cleanup

--- src/coloring_book/UploadFilePage.jsx
+++ src/coloring_book/UploadFilePage.jsx
@@ -17,8 +17,11 @@ export default function UploadFilePage({ onComplete }) {
   const [imageState, setImageState] = useState(null);
   const [filenameState, setFilenameState] = useState(null);
   const [canStartProcessing, setCanStartProcessing] = useState(false);
-  const [canStartDrawing, setCanStartDrawing] = useState(false);
+  const [canStartPainting, setCanStartPainting] = useState(false);
   const [currentStatus, setCurrentStatus] = useState('')
+  const [doKuwahara, setDoKuwahara] = useState(true)
+  const [doGaussian, setDoGaussian] = useState(true)
+  const [doClustering, setDoClustering] = useState(true)
 
   const delayWithDraw = async (ms) => {
     await new Promise(requestAnimationFrame);
@@ -27,8 +30,9 @@ export default function UploadFilePage({ onComplete }) {
 
   const processImageFromCanvas = async (canvas, filename) => {
     setCanStartProcessing(false);
-    setCanStartDrawing(false);
+    setCanStartPainting(false);
     setImageState(null);
+    const usedClustering = doClustering;
     const img = new Image();
     img.onload = async () => {
       const ctx = canvasRef.current.getContext("2d");
@@ -37,34 +41,34 @@ export default function UploadFilePage({ onComplete }) {
       // setImageDimensions({ width: img.width, height: img.height });
 
       ctx.drawImage(img, 0, 0);
-      setCurrentStatus('Applying Kuwahara Filter...')
-      await delayWithDraw(100);
+      if (doKuwahara) {
+        setCurrentStatus('Applying Kuwahara Filter...')
+        await delayWithDraw(10);
+  
+        KuwaharaFilter(ctx, img.width, img.height);
+      }
 
-      KuwaharaFilter(ctx, img.width, img.height);
-      setCurrentStatus('Blurring Edges...')
-      await delayWithDraw(800);
+      if (doGaussian) {
+        setCurrentStatus('Blurring Edges...')
+        await delayWithDraw(800);
+  
+        GaussianBlur(ctx, img.width, img.height, 2);
+      }
+      
+      if (doClustering) {
+        setCurrentStatus('Clustering Colors...')
+        await delayWithDraw(800);
+  
+        KMeansTransformation(ctx, img, numColors);
+      }
+      await delayWithDraw(10);
 
-      GaussianBlur(ctx, img.width, img.height, 2);
-      setCurrentStatus('Clustering Colors...')
-      await delayWithDraw(800);
-
-      KMeansTransformation(ctx, img, numColors);
-      await delayWithDraw(800);
-
-      // extractRegions(
-      //   ctx,
-      //   setColors,
-      //   setRegionMap,
-      //   SMALL_REGION_THRESHOLD,
-      //   onComplete,
-      //   img,
-      //   filename
-      // );
-
-      setCurrentStatus('Done!')
+      setCurrentStatus('Done! (except for small region combining)')
       setImageState(img);
       setFilenameState(filename);
-      setCanStartDrawing(true); 
+      if (usedClustering) {
+        setCanStartPainting(true); 
+      }
     };
     img.src = canvas.toDataURL();
   };
@@ -122,7 +126,7 @@ export default function UploadFilePage({ onComplete }) {
     }
 
     setCanStartProcessing(true);
-    setCanStartDrawing(false);
+    setCanStartPainting(false);
     setCurrentStatus('')
   };
 
@@ -144,7 +148,7 @@ export default function UploadFilePage({ onComplete }) {
   };
 
 
-  const startDrawing = () => {
+  const startPainting = () => {
     const ctx = canvasRef.current.getContext("2d")
     extractRegions(
       ctx,
@@ -158,49 +162,62 @@ export default function UploadFilePage({ onComplete }) {
   }
 
   return (
-    <div className="flex h-[100vh] w-full p-6">
+    <div className="flex h-[100vh] w-full p-15">
       {/* Left side */}
-      <div className="w-2/5 pr-6 flex flex-col space-y-6">
+      <div className="w-auto pr-15 flex flex-col items-center space-y-6 whitespace-nowrap border-r-2 border-black/30 border-dotted">
         <h1 className="text-5xl font-semibold">Region Extractor</h1>
-
-        <div className="flex gap-6">
-          <label>
-            Small Region Threshold:
-            <input
-              type="number"
-              min={1}
-              max={200}
-              value={SMALL_REGION_THRESHOLD}
-              onChange={(e) =>
-                set_SMALL_REGION_THRESHOLD(Math.max(1, Math.min(200, Number(e.target.value))))
-              }
-              className="no-spinner ml-2 border-b outline-0 text-center"
-            />
-          </label>
-
-          <label>
-            Number of Colors:
-            <input
-              type="number"
-              min={-1}
-              max={30}
-              value={numColors}
-              onChange={(e) =>
-                setNumColors(Math.max(-1, Math.min(256, Number(e.target.value))))
-              }
-              className="no-spinner ml-2 border-b outline-0 text-center"
-            />
-          </label>
+        <div>
+          <div className="flex gap-6">
+            <label className=''>
+              Small Region Threshold:
+              <input
+                type="number"
+                min={1}
+                max={200}
+                value={SMALL_REGION_THRESHOLD}
+                onChange={(e) =>
+                  set_SMALL_REGION_THRESHOLD(Math.max(1, Math.min(200, Number(e.target.value))))
+                }
+                className="no-spinner ml-2 border-b outline-0 text-center"
+              />
+            </label>
+            <label>
+              Number of Colors:
+              <input
+                type="number"
+                min={0}
+                max={30}
+                value={numColors}
+                onChange={(e) =>
+                  setNumColors(Math.max(0, Math.min(256, Number(e.target.value))))
+                }
+                className="no-spinner ml-2 border-b outline-0 text-center"
+              />
+            </label>
+          </div>
+          <div className="flex w-full gap-6 items-center justify-center">
+            <div className={`mt-4 px-2 py-1 rounded-lg mb-3 border border-gray-400 flex items-center gap-2 cursor-pointer ${doKuwahara ? "bg-green-200" : "bg-white"}`} onClick={() => setDoKuwahara(prev => !prev)}>
+              Kuwahara
+            </div>
+            {"->"}
+            <div className={`mt-4 px-2 py-1 rounded-lg mb-3 border border-gray-400 flex items-center gap-2 cursor-pointer ${doGaussian ? "bg-green-200" : "bg-white"}`} onClick={() => setDoGaussian(prev => !prev)}>
+              Gaussian
+            </div>
+            {"->"}
+            <div className={`mt-4 px-2 py-1 rounded-lg mb-3 border border-gray-400 flex items-center gap-2 cursor-pointer ${doClustering ? "bg-green-200" : "bg-white"}`} onClick={() => setDoClustering(prev => !prev)}>
+              K-Means*
+            </div>
+          </div>
+          <div className='text-center text-xs'>*kmeans necessary for painting</div>
         </div>
-        
         <div className='h-full flex flex-col items-center justify-items-center justify-center space-y-15'>
           <div
-            className="flex flex-col items-center justify-center h-[60vh] border-4 border-dashed border-gray-300 rounded-lg p-8 text-center cursor-pointer hover:bg-gray-50 transition"
+            className="flex flex-col items-center justify-center h-full border-4 border-dashed border-gray-300 rounded-lg p-8 text-center cursor-pointer hover:bg-gray-50 transition"
             onClick={() => fileInputRef.current?.click()}
             onDrop={handleDrop}
             onDragOver={handleDragOver}
           >
-            <p className="text-gray-600 text-xl">Click or drag-and-drop an image or JSON file here</p>
+            <p className="text-gray-600 text-xl whitespace-normal text-center max-w-xs">Click or drag-and-drop an image or JSON file here</p>
             <input
               ref={fileInputRef}
               type="file"
@@ -217,10 +234,10 @@ export default function UploadFilePage({ onComplete }) {
               Start Processing
             </button>
             <button
-              onClick={startDrawing}
-              className={`px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition ${canStartDrawing ? '' : 'hidden'}`}
+              onClick={startPainting}
+              className={`px-6 py-2 bg-green-600 text-white rounded-lg hover:bg-green-700 transition ${canStartPainting ? '' : 'hidden'}`}
             >
-              Start Drawing
+              Start Painting
             </button>
           </div>
         </div>
@@ -243,11 +260,11 @@ ${regionMap.map(region =>
       </div>
 
       {/* Right side */}
-      <div className="w-3/5">
+      <div className="w-full h-full flex flex-col justify-center text-center overflow-hidden">
         <div className='text-center'><h1 className='text-5xl font-semibold pb-5'>Preview</h1></div>
-        <div className={`h-full flex flex-col justify-center items-center`}>
+        <div className={`h-full overflow-hidden flex flex-col justify-center items-center overflow-y-scroll`}>
           {currentStatus}
-          <canvas ref={canvasRef} className="border shadow-md" />
+          <canvas ref={canvasRef} className="border shadow-md mt-5" />
         </div>
       </div>
     </div>
